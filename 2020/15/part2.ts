934126a8922e5c0e91490c967b64349c44c77b29
Magnus Johansson <magnus.johansson@se.ibm.com>
2020-12-15 11:16:18 +0100

    Cleanup

--- 2020/15/part2.ts
+++ 2020/15/part2.ts
@@ -1,4 +1,4 @@
-import { range } from 'lib/arrays';
+import { fill, range } from 'lib/arrays';
 import { readFile } from '../../lib/readFile';
 
 type Input = {
@@ -10,25 +10,19 @@ export const parse = (input: string[]): Input => {
 }
 
 export const solve = (input: Input): number => {
-  const lastPosition: Map<number, number> = new Map<number, number>();
+  const positions = new Int32Array(30000000);
 
   for (let i = 0; i < input.values.length; i++) {
-    lastPosition.set(input.values[i], i);
+    positions[input.values[i]] = i + 1;
   }
 
   let last = input.values[input.values.length - 1];
-  let lp = -1;
-  for (let i = input.values.length; i < 30000000; i++) {
-    //if (i % 1000000 === 0) console.log(i, last, Object.keys(lastPosition).length);
+  let lastPosition = 0;
+  for (let i = input.values.length; i < positions.length; i++) {
+    last = lastPosition ? i - lastPosition : 0;
 
-    if (lp != -1) {
-      last = i - 1 - lp;
-    } else {
-      last = 0;
-    }
-    const b = (lastPosition.has(last) ? lastPosition.get(last) : -1)!;
-    lp = b;
-    lastPosition.set(last, i);
+    lastPosition = positions[last];
+    positions[last] = i + 1;
   }
 
   return last;
@@ -38,4 +32,6 @@ export const solve = (input: Input): number => {
 //console.log(solve({ values: [1, 2, 3]}));
 //console.log(solve({ values: [1, 3, 2]}));
 
-console.log(solve(parse(readFile(__dirname))));
+for (let i = 0; i < 10; i++) {
+  console.log(solve(parse(readFile(__dirname))));
+}
